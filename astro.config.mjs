437b55b383612202bcc5b396315c9fa8100d7872
Your Name¨ <you@example.com>
2025-12-29 20:48:51 +0100

    Add custom domain and analytics setup

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,7 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://pscllbssr.github.io',
+  site: 'https://pscl.lbssr.ch',
   // Uncomment and set if deploying to a repository other than username.github.io:
   // base: '/2026-portfolio',
 });
